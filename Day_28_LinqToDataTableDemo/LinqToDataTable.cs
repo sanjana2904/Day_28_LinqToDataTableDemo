3002bc47b00ee5422344516b0c89c25790aeea47
sanjana k <sanjanarthi@gmail.com>
2021-04-20 20:51:25 +0530

    Add project files.

---
+++ Day_28_LinqToDataTableDemo/LinqToDataTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Day_28_LinqToDataTableDemo
+{
+    class LinqToDataTable
+    {
+        public void AddToDataTableDemo()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID");
+            table.Columns.Add("ProductName");
+
+            table.Rows.Add("1", "Chai");
+            table.Rows.Add("2", "Queso Cabrels");
+            table.Rows.Add("3", "Tofu");
+            DisplayProductsFromTable(table);
+        }
+
+        public void DisplayProductsFromTable(DataTable table)
+        {
+            var productNames = from products in table.AsEnumerable() select products.Field<string>("ProductName");
+            Console.WriteLine("Product Names: ");
+            foreach (string productName in productNames)
+            {
+                Console.WriteLine(productName);
+            }
+        }
+
+    }
+}
